225456cdef8be5ac5254c259589058db222001c6
macoto35 <h353535@hotmail.com>
2018-04-10 22:14:57 +0800

    Update LastDigitPartialSumOfFiboNum.py

---
+++ python/LastDigitPartialSumOfFiboNum.py
@@ -0,0 +1,46 @@
+
+def naive(m, n):
+    
+    sum = 0
+    current = 0
+    next = 1
+
+    for i in range(n + 1):
+        if i >= m:
+            sum += current
+        current, next = next, (current + next)
+
+    return sum % 10
+
+
+def fast(m, n):
+    if n <= 1:
+        return n
+
+    a = getFibo(n + 2)
+    b = getFibo(m + 1)
+    
+    return a - b if a >= b else a - b + 10
+
+def setFibo():
+    arr = []
+    arr.append(0)
+    arr.append(1)
+    i = 2
+
+    while(1):
+        arr.append( (arr[i - 1] + arr[i - 2]) % 10 )
+        if arr[i - 1] == 0 and arr[i] == 1:
+            break
+        else:
+            i += 1
+    return arr[:i - 1]
+
+def getFibo(v):
+    a = arr[ v % len(arr) ]
+    return 9 if a == 0 else a - 1
+
+m, n = map(int, input().split())
+arr = setFibo()
+print("naive: ", naive(m, n))
+print("fast: ", fast(m, n))
