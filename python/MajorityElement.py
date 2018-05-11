37c7c72c0d7dcd8acdfc77c786370a8b5f28245d
sohee.um <sohee.um@mercer.com>
2018-05-11 17:08:54 +0800

    python - majority elemt & org lottery

--- python/MajorityElement.py
+++ python/MajorityElement.py
@@ -1,4 +1,6 @@
 
+from random import *
+
 def naive(arr):
   count = 0
   cur = 0
@@ -12,6 +14,7 @@ def naive(arr):
     if count > len(arr)/2:
       return 1
   return 0
+
 
 def greedy(arr):
   mid = len(arr)/2
@@ -33,21 +36,75 @@ def greedy(arr):
       return 1
   return 0
 
+
+def scan(arr, size):
+  elemt = -1
+  cnt = 0
+  
+  while len(arr) > 0:
+    x = arr[0]
+    i = 0
+    cnt = 0
+    while len(arr) > i and len(arr) > 0:
+      if x == arr[i]:
+        arr.pop(i)
+        cnt += 1
+      else:
+        i += 1
+    if cnt > size // 2:
+      elemt = x
+      break
+
+  return elemt
+
+def findMajority(arr, st, ed, a, b):
+  if a == b:
+    return a
+  else:
+    return scan(arr[st:ed + 1], ed - st + 1)
+
 def divideAndConquer(arr, st, ed):
-  print('enter: ',st,ed)
   if st == ed:
-    print('return=====', arr[st])
     return arr[st]
   
-  mid = st + (ed - st) // 2
+  m = st + (ed - st) // 2
   
-  a = divideAndConquer(arr, st, mid)
-  b = divideAndConquer(arr, mid + 1, ed)
-  
-  print('next-------- ', a, b)
+  a = divideAndConquer(arr, st, m)
+  b = divideAndConquer(arr, m + 1, ed)
+  return findMajority(arr, st, ed, a, b)
 
-arr = [1,2,3,1,1]
+'''
+arr = [2,2,3,7,7]
 arr1 = [1,2,3,1,1]
-#print(naive(arr))
-#print(greedy(arr1))
-print(divideAndConquer(arr, 0, len(arr)-1))
+print(naive(arr))
+print(greedy(arr1))
+print(divideAndConquer(arr, 0, len(arr1) - 1))
+'''
+
+'''
+N, I = input().split()
+while True:
+  n = randint(1, int(N))
+  arr = []
+  for i in range(n):
+    arr.append(randint(0, int(I)))
+  arr1 = arr[0:len(arr)]
+  
+  print(arr, arr1, end = ' ')
+  r1 = greedy(arr)
+  r2 = 1 if divideAndConquer(arr1, 0, len(arr1) - 1) > -1 else 0
+  
+  if (r1 == r2):
+    print(' - pass!')
+  else:
+    print(' - fail!')
+    break
+'''
+
+file = open('sample/4_2_majority_element.in', 'r')
+n = file.readline()
+arr = [*map(int, file.readline().split())]
+
+#print(greedy(arr))
+print(divideAndConquer(arr, 0, len(arr) - 1))
+
---
+++ python/organizingLottery.py
@@ -0,0 +1,21 @@
+'''
+def naiveOrgLottery(segs, nums):
+  result = []
+  sum = 0
+
+  for n in nums:
+    sum = 0
+    for s in segs:
+      if (s[0] <= n and s[1] >= n):
+        sum += 1
+    result.append(sum)
+
+  return result
+print(*naiveOrgLottery(segs, nums))
+'''
+
+
+s,p = map(int, input().split())
+segs = []
+exec('segs.append([*map(int, input().split())]); ' * s)
+nums = [*map(int, input().split())]
